fea8eb9b36fdfd30d71fafd43f77026da50ef213
Abdulhadi-assi <178458269+Abdulhadi-Assi@users.noreply.github.com>
2025-01-10 15:42:26 +0300

    adding Update Restaurant Details function

--- src/main/java/food_delivery/service/impl/RestaurantDetailsServiceImpl.java
+++ src/main/java/food_delivery/service/impl/RestaurantDetailsServiceImpl.java
@@ -1,12 +1,12 @@
 package food_delivery.service.impl;
 
 import org.springframework.stereotype.Service;
-
 import food_delivery.model.RestaurantDetails;
 import food_delivery.repository.RestaurantDeatailsRepository;
 import food_delivery.request.RestaurantRequest;
 import food_delivery.service.RestaurantDetailsService;
-import org.springframework.stereotype.Service;
+import food_delivery.request.RestaurantDetailsRequest;
+import java.util.Optional;
 
 
 @Service
@@ -18,6 +18,20 @@ public class RestaurantDetailsServiceImpl implements RestaurantDetailsService {
     public RestaurantDetails getRestaurantDetails(Long id) {
         return null;
     }
+
+	@Override
+	public RestaurantDetails UpdateRestaurantDetails(RestaurantDetails restaurantDetails, RestaurantDetailsRequest restaurantDetailsRequest) {
+
+		if(restaurantDetailsRequest == null)
+		{
+			return restaurantDetails;
+		}
+
+		Optional.ofNullable(restaurantDetailsRequest.getDescription()).ifPresent(restaurantDetails::setDescription);
+		Optional.ofNullable(restaurantDetailsRequest.getCapacity()).ifPresent(restaurantDetails::setCapacity);
+
+		return restaurantDeatailsRepository.save(restaurantDetails);
+	}
 
 	@Override
 	public RestaurantDetails createRestaurantDetails(RestaurantRequest req) {
